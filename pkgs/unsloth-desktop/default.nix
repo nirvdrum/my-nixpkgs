b1f42ff23b3c66af059ad98707b56b0b82b7996a
Kevin Menard <kevin@nirvdrum.com>
2026-09-22 20:00:41 -0400

    Update Unsloth Desktop from 0.1.813-beta -> 0.1.814-beta.

--- pkgs/unsloth-desktop/default.nix
+++ pkgs/unsloth-desktop/default.nix
@@ -13,14 +13,14 @@
 
 let
   pname = "unsloth-desktop";
-  version = "0.1.813-beta";
+  version = "0.1.814-beta";
 in
 
 if stdenvNoCC.hostPlatform.isLinux then
   let
     src = fetchurl {
       url = "https://github.com/unslothai/unsloth/releases/download/v${version}/Unsloth-Desktop-Linux.AppImage";
-      hash = "sha256-jXk62ScgzuF0DBOwHdudFvbgPaD9F7dhcpjv0m1qhjk=";
+      hash = "sha256-ifEJbtyVZe6eki+tAel0yMJ03xuVEvx0hvqBxoY9PkI=";
     };
 
     appimageContents = appimageTools.extractType2 { inherit pname version src; };
@@ -198,7 +198,7 @@ else
 
     src = fetchurl {
       url = "https://github.com/unslothai/unsloth/releases/download/v${version}/Unsloth-Desktop-MacOS.dmg";
-      hash = "sha256-KMfm2UrcHWgtRg+nwtHuRnhVxiQfPXBkC61FDVOaZ7Q=";
+      hash = "sha256-+R2LFe3HcIfLL77BjGIrf96+ib4Ut+PhtDzUaqB1MyQ=";
     };
 
     nativeBuildInputs = [ undmg ];
